7d3c84633c83fb6c520df173e3d62001de324600
gilcllys <gilcllyscosta@gmail.com>
2022-03-07 21:56:11 -0400

    funcao de gatilho funcionando

--- js/interruptor.js
+++ js/interruptor.js
@@ -1,1 +1,24 @@
- 
+function gatilho(btn){
+    switch (btn.id){
+        case "btn-1":
+            let a1 = document.getElementById("a-1")
+            a1.classList.toggle("showup") 
+            break
+        case "btn-2":
+            let a2 = document.getElementById("a-2")
+            a2.classList.toggle("showup") 
+            break
+        case "btn-3":
+            let a3 = document.getElementById("a-3")
+            a3.classList.toggle("showup") 
+            break
+        case "btn-4":
+            let a4 = document.getElementById("a-4")
+            a4.classList.toggle("showup") 
+            break
+        case "btn-5":
+            let a5 = document.getElementById("a-5")
+            a5.classList.toggle("showup") 
+            break
+    }    
+} 
